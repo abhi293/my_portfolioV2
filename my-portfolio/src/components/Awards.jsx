e018c9360bf5f98cc17a15caf268254d01f6fe3a
Abhinav Anand <anandabhinav293@gmail.com>
2025-03-27 23:22:12 +0530

    V2

--- my-portfolio/src/components/Awards.jsx
+++ my-portfolio/src/components/Awards.jsx
@@ -2,40 +2,40 @@ import "../styles/Awards.css";
 import p5 from "../assets/p5.jpg";
 import p6 from "../assets/p6.jpg";
 import p7 from "../assets/p7.png";
-import p8 from "../assets/p8.jpg";
-import p9 from "../assets/p9.jpg";
+import p8 from "../assets/p8.png";
+import p9 from "../assets/p9.png";
 const Awards = () => {
   const certifications = [
     {
       icon: "ri-medal-fill",
-      title: "Learning Git and GitHub",
-      issuer: "LinkedIn",
-      date: "Completed: June 2024",
-      link: "https://www.linkedin.com/learning/certificates/aeda61d6ba2071c04f874c0a147144737430680596afac2363bf34ae69cabc4f?trk=share_certificate",
+      title: "Python Full Stack Development",
+      issuer: "eduSkills",
+      date: "Completed: Dec 2024",
+      link: "https://www.linkedin.com/in/abhinav-anand-9460b0200/details/certifications/1740436842127/single-media-viewer/?profileId=ACoAADNFgEYBvWoUO8JxeQ22oHU62Kbzfkujkisg",
       image: p5,
     },
     {
       icon: "ri-code-s-slash-line",
-      title: "Career Essentials in Software Development",
+      title: "Microsoft Azure Fundamentals",
       issuer: "Microsoft & LinkedIn",
-      date: "Completed: May 2024",
-      link: "https://www.linkedin.com/learning/certificates/44fabd3b2484caec9dec7473c9c0fa2bc19b847c0791345d1db1ca8f26bf3076?trk=share_certificate",
+      date: "Completed: Feb 2025",
+      link: "https://www.linkedin.com/in/abhinav-anand-9460b0200/details/certifications/1740426510251/single-media-viewer/?profileId=ACoAADNFgEYBvWoUO8JxeQ22oHU62Kbzfkujkis",
       image: p6,
     },
     {
       icon: "ri-award-fill",
-      title: "Legacy JavaScript Algorithms and Data Structures",
-      issuer: "freeCodeCamp",
-      date: "Completed: May 2024",
-      link: "https://www.freecodecamp.org/certification/Nikhil_Soni/javascript-algorithms-and-data-structures",
+      title: "JavaScript Algorithms and Data Structures",
+      issuer: "Scaler",
+      date: "Completed: June 2024",
+      link: "https://moonshot.scaler.com/s/sl/ZGB_GzkUEu",
       image: p7,
     },
     {
       icon: "ri-node-tree",
       title: "Node.js Essential Training",
-      issuer: "LinkedIn",
-      date: "Completed: May 2024",
-      link: "https://www.linkedin.com/learning/certificates/a9573d2894abd382da006d80f5cd04407e1b1891893f884172ce21daad12074b?trk=share_certificate",
+      issuer: "Scaler",
+      date: "Completed: Mar 2025",
+      link: "https://www.linkedin.com/in/abhinav-anand-9460b0200/details/certifications/1742369985924/single-media-viewer/?profileId=ACoAADNFgEYBvWoUO8JxeQ22oHU62Kbzfkujkis",
       image: p8,
     },
     {
@@ -43,7 +43,7 @@ const Awards = () => {
       title: "6-Week C++ Course Online with Certificate",
       issuer: "GeeksforGeeks",
       date: "Issued: Jan 2025",
-      link: "https://www.geeksforgeeks.org/certificate/bc58c7bf649ae1973cba2e7a2170ac44",
+      link: "https://www.geeksforgeeks.org/certificate/392741464116b198d05c31d0b47b6ede?utm_source=socials&utm_medium=cc_link",
       image: p9,
     },
   ];
@@ -72,7 +72,7 @@ const Awards = () => {
           ))}
         </div>
         <a
-          href="https://www.linkedin.com/in/nikhilsoni973"
+          href="https://www.linkedin.com/in/abhinav-anand-9460b0200/"
           target="_blank"
           rel="noopener noreferrer"
           className="text-blue-500"
